8b1e5cc0980ac38a9cd005cd75966145c99d4d29
qa-qrugy-4-quyb@example.com <qa-qrugy-4-quyb@example.com>
2023-03-08 19:29:07 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjEzMzU4MQ/spectral.js?branch_slug=main&token=59cf7c8e-fa03-4bdf-8c84-46cf7be652b2";
+export default { extends: ruleset };
